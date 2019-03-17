fcc316a19685c775019a6d41365ff570ad92e1fa
zxxgood <34737163+zxxgood@users.noreply.github.com>
2019-03-17 22:44:30 +0800

    Add files via upload
    部落707电脑里面的dy项目代码，提交时间：2019-03-17

---
+++ dy_parent/src/main/java/com/example/dy_parent/DyParentApplication.java
@@ -0,0 +1,14 @@
+package com.example.dy_parent;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyParentApplication.class, args);
+	}
+
+}
